6c8c773dcb53fb73b55f28ab1ff8d6ab8f1c124f
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-08-20 01:03:42 +0530

    update

---
+++ react/07-Cards/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const[cards, setCards] = useState([])
+
+  const fetchData = asyn() => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    setCards(data)
+    console.log(data)
+  }
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
