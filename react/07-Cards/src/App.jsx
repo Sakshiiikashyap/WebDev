933ddc3be8786774e6c46ff8f7dead7ac3167bee
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-08-20 10:32:57 +0530

    update

--- react/07-Cards/src/App.jsx
+++ react/07-Cards/src/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const[cards, setCards] = useState([])
 
-  const fetchData = asyn() => {
+  const fetchData = async () => {
     let a = await fetch("https://jsonplaceholder.typicode.com/posts")
     let data = await a.json()
     setCards(data)
     console.log(data)
   }
 
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <>
-      
+    <Navbar/>
+      <div className="container">
+    {cards.map((card, index) => {
+      return (
+        <div key={card.id || index} className="card">
+          <h1>{card.title}</h1>
+          <p>{card.body}</p>
+          <span>by: userId: {card.userId}</span>
+        </div>
+      );
+    })}
+  </div>
     </>
   )
 }
